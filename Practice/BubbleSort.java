f1705c6947bd40631ba9454d8f4a9a07a62cc984
aasif-create <aasifmmd12345@gmail.com>
2026-06-15 23:21:12 +0530

    UPDATED

--- Practice/BubbleSort.java
+++ Practice/BubbleSort.java
@@ -3,6 +3,12 @@ public class BubbleSort
     public static void main(String[] args)
     {
       int[] arr = {40,20,700,90,50};
+      System.out.println("THE ARRAY ELEMENTS ARE : ");
+      for(int i=0;i<arr.length;i++)
+      {
+        System.out.print(arr[i] + " ");
+      }
+      System.out.println();
       for(int i=0;i<arr.length;i++)
       {
         for(int j=0;j<arr.length -1 ;j++)
