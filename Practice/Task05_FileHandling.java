52f83291eacb73270104ebff3ef8871dc9ae17b5
aasif-create <aasifmmd12345@gmail.com>
2026-06-18 23:23:53 +0530

    PRACTICED FILE HANDLING IN JAVA

---
+++ Practice/Task05_FileHandling.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Task05_FileHandling
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            // Writing to file
+            FileWriter fw = new FileWriter("data.txt");
+            fw.write("Hello from Java File Handling");
+            fw.close();
+
+            // Reading from file
+            FileReader fr = new FileReader("data.txt");
+
+            int ch;
+
+            System.out.println("FILE CONTENTS:");
+
+            while((ch = fr.read()) != -1)
+            {
+                System.out.print((char)ch);
+            }
+
+            fr.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
